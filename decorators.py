0c0fabc4e0afe1b85a57ae0cdc07f831d3a53adc
Philipp Weinmann <philipp.weinmann71@gmail.com>
2024-06-25 21:37:54 +0200

    added memory decorator

--- decorators.py
+++ decorators.py
@@ -1,5 +1,7 @@
 from functools import wraps
 import time
+import tracemalloc
+
 
 # decorator function to measure time taken of fct.
 def timeit(func):
@@ -13,3 +15,23 @@ def timeit(func):
         print(f'{func.__name__} Function took {total_time:.4f}s')
         return result
     return timeit_wrapper
+
+def measure_memory_usage(func):
+    def wrapper(*args, **kwargs):
+        tracemalloc.start()
+
+        # Call the original function
+        result = func(*args, **kwargs)
+
+        snapshot = tracemalloc.take_snapshot()
+        top_stats = snapshot.statistics("lineno")
+
+        # Print the top memory-consuming lines
+        print(f"Memory usage of {func.__name__}:")
+        for stat in top_stats[:5]:
+            print(stat)
+
+        # Return the result
+        return result
+
+    return wrapper
